cf3eb9645ce97e06730b787e6d29ffc4e7408832
nivetha.samraj <nivetha0997@gmail.com>
2019-12-03 10:08:41 +0530

    sine_h commit

--- src/driver/scientific_calc.py
+++ src/driver/scientific_calc.py
@@ -1,15 +1,20 @@
 import logging
-logging.basicConfig(level=logging.exception('Exception occured'))
 
+logging.basicConfig(filename = 'ScientificCalculatorLog.log', level=logging.ERROR,
+                    format = '%(name)s - %(levelname)s - %(message)s - %(asctime)s '
+                           '- %(lineno)d - %(module)s - %('
+                           'funcName)s - %(pathname)s')
 """MODULE IS USED TO FIND SINH VALUE"""
 
 
 class ScientificCalc:
     def __init__(self):
+
         self.sinh_of_x=0
 
     @classmethod
     def sine_h(cls, x_value):
+
         """formula for finding sin"""
         exp_value = 2.71828
         try:
